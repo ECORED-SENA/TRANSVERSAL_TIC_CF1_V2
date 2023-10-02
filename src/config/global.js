d45c63e30b5174e4c8cf43b82ca5d0cf1c2c2c92
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-10-02 09:25:05 -0500

    Create material.zip

--- src/config/global.js
+++ src/config/global.js
@@ -144,11 +144,6 @@ export default {
         nombreRuta: 'referencias',
       },
       {
-        icono: 'fas fa-file-pdf',
-        titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
-      },
-      {
         icono: 'fas fa-download',
         titulo: 'Descargar material',
         download: 'downloads/material.zip',
